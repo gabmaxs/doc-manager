e5e420fb80e0835a384c4ed4632382c3c6199a2f
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-02-09 17:19:15 -0300

    add(seeders): add seeders to database seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            VersionSeeder::class,
+            SectionSeeder::class,
+            PageSeeder::class,
+            CategorySeeder::class
+        ]);
     }
 }
