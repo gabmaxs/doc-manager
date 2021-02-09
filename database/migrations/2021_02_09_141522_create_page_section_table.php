2f966a485fd01fb7d8c8f7e84d1967c49e95c8d5
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-02-09 11:28:10 -0300

    add(migration-page-section): add page-section migration

---
+++ database/migrations/2021_02_09_141522_create_page_section_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePageSectionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_section', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("page_id");
+            $table->bigInteger("section_id");
+            $table->boolean("is_active")->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('page_section');
+    }
+}
