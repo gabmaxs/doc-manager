cb91fb2eb9af46129b548ab01307d78d927148a5
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-02-09 11:49:13 -0300

    add(model-page): add page model

---
+++ app/Models/Page.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["name"];
+
+    public function sections() {
+        return $this->belongsToMany(Section::class);
+    }
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
+}
