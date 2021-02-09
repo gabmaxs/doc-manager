05c4d63c824505aad8c50ee5add4d207f70f59d6
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-02-09 11:47:29 -0300

    add(model-section): add section model

---
+++ app/Models/Section.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Section extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["name", "content"];
+
+    public function versions() {
+        return $this->belongsToMany(Version::class);
+    }
+
+    public function pages() {
+        return $this->belongsToMany(Page::class);
+    }
+}
