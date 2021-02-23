984b192210819b598c2861adabd61d8aab6f8d12
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-02-23 11:02:11 -0300

    fix(docs-sidebar): fix links in sidebar

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -12,17 +12,13 @@
         <nav id="docs-nav" class="docs-nav navbar">
         <ul class="section-items list-unstyled nav flex-column pb-3">
                 @foreach($categories as $category)
-				<li class="nav-item section-title"><a class="nav-link" href="#{{str_replace(' ', '', $category->name)}}" data-toggle="collapse" aria-expanded="false">
+				<li class="nav-item section-title"><a class="nav-link" href="#{{ $category->title }}" data-toggle="collapse" aria-expanded="false">
                     <span class="theme-icon-holder mr-2">
                     <i class="{{$category->icon}}"></i></span>{{$category->name}}
                 </a></li>
-  					<ul class="collapse list-unstyled" id="{{str_replace(' ', '', $category->name)}}">
+  					<ul class="collapse list-unstyled" id="{{ $category->title }}">
                     @foreach ($category->pages as $pageMenu)
-                        @if ($category->is($page->category))
-                            <li class="nav-item"><a class="nav-link scrollto active" href="#{{ $pageMenu->title }}">{{$pageMenu->name}}</a></li>
-                        @else
-                            <li class="nav-item"><a class="nav-link" href="{{ route('docs',['version' => $currentVersion, 'page' => $pageMenu]) . "#$pageMenu->title" }}">{{$pageMenu->name}}</a></li>
-                        @endif
+                        <li class="nav-item"><a class="nav-link" href="{{ route('docs',['version' => $currentVersion, 'page' => $pageMenu]) }}">{{$pageMenu->name}}</a></li>
                     @endforeach
 					</ul> 
                 @endforeach
